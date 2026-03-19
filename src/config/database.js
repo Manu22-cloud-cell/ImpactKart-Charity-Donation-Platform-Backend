a61e0d34bc61950a01ef5e488fd71e24cb4eff24
Manu22-cloud-cell <mailtomanojky@gmail.com>
2026-03-19 14:12:05 +0530

    database config changes

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,14 @@
-const {Sequelize} = require("sequelize");
+const { Sequelize } = require("sequelize");
 
-const sequelize=new Sequelize(
+const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host:process.env.DB_HOST,
-        dialect:process.env.DB_DIALECT || "mysql",
-        logging:false,
+        host: process.env.DB_HOST,
+        dialect: "mysql",
+        logging: false,
     }
 );
 
-module.exports=sequelize;
+module.exports = sequelize;
