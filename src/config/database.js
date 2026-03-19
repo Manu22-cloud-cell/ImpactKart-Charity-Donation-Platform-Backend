1e27778816c8788f3d6bd8d11ce93d8482e1852a
Manu22-cloud-cell <mailtomanojky@gmail.com>
2026-03-19 16:32:20 +0530

    Debug env

--- src/config/database.js
+++ src/config/database.js
@@ -13,4 +13,11 @@ const sequelize = new Sequelize(
 
 console.log("DB CONFIG LOADED - DIALECT:", "mysql");
 
+console.log("ENV DEBUG:", {
+    DB_NAME: process.env.DB_NAME,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD ? "SET" : "MISSING",
+    DB_HOST: process.env.DB_HOST,
+});
+
 module.exports = sequelize;
